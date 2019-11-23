afe39dbfbda1166abf51bc163619812ea33356f4
Adrian Brodzik <adrianbrodzyk@gmail.com>
2019-11-23 15:50:18 +0100

    Add benchmark functions

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 import argparse
+import math
 import time
 
 from src.model import *
 from src.utility import *
-
-
-def fitness_function(data):
-    score = 0
-    for bit in data:
-        if bit == 1:
-            score += 1
-    return score
+from src.benchmark import *
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--world_size", type=int, default=1)
+    parser.add_argument("--world_size", type=int, default=4)
     parser.add_argument("--population_size", type=int, default=500)
-    parser.add_argument("--individual_size", type=int, default=100)
+    parser.add_argument("--individual_size", type=int, default=8*40)
     parser.add_argument("--mutation_rate", type=float, default=1e-3)
-    parser.add_argument("--migration_interval", type=int, default=0)
-    parser.add_argument("--migration_size", type=int, default=0)
-    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--migration_interval", type=int, default=200)
+    parser.add_argument("--migration_size", type=int, default=1)
+    parser.add_argument("--seed", type=int, default=42)
 
     args = parser.parse_args()
 
@@ -33,9 +27,9 @@ def main():
                   args.mutation_rate,
                   args.migration_interval,
                   args.migration_size,
-                  fitness_function)
-    world.run(10000)
-    print(world.islands[0].individuals[0])
+                  rastrigin)
+    best_individual = world.run_parallel(10000000, 0)
+    print(best_individual)
 
 
 if __name__ == "__main__":
